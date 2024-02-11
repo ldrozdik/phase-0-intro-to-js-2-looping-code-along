7fb3cc2e4b11d14bc0f10da6ec0d912e32a5ac4f
Lindsay Drozdik <linds.drozdik@yahoo.com>
2024-02-11 13:38:59 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    let thankYouMessages = []
+    for (let i = 0; i < cards.length; i++) {
+        thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+
+const messages = writeCards(cards);
+console.log(messages)
+
+function countDown() {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+countDown(10);
